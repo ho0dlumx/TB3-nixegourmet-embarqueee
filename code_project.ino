eeac04c0af83c677d5c576ffdec0d62713e5d38d
Camilla Rosa Freire Sousa <73553875+ho0dlumx@users.noreply.github.com>
2024-01-11 18:44:04 +0100

    Update code_project.ino

--- code_project.ino
+++ code_project.ino
@@ -2,8 +2,8 @@
 #include <HTTPClient.h>
 #define LED 26
 
-const char* ssid = "Wi-Fi Maison des Élèves";
-const char* password = "w1f1delaME139";
+const char* ssid = "Wi-Fi Maison des Élèves"; //Wifi network name
+const char* password = "w1f1delaME139"; //Wifi network password
 
 void setup() {
   Serial.begin(9600);
@@ -23,17 +23,17 @@ void setup() {
 
 void loop() {
   if (Serial.available() > 0) {
-    String barcode = Serial.readStringUntil('\n'); //
+    String barcode = Serial.readStringUntil('\n'); //barcode entry with scan
     if (barcode.length() > 0) {
       HTTPClient http;
       //String url = "http://localhost:8000/integration/"+barcode+".html";
       //String url = "http://IP:8000/integration/"+barcode+".html";
-      String url = "http://linkedin.";
+      String url = "http://linkedin." + barcode; //test to see if code works
       http.begin(url);
       int httpCode = http.GET();
       if (httpCode > 0) {
         Serial.println("Page opened: " + url);
-        digitalWrite(LED,HIGH);
+        digitalWrite(LED,HIGH); #visual test
       } else {
         Serial.println("Error opening page");
       }
