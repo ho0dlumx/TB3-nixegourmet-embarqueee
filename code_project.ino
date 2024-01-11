68232fd1b4ffb9db85877f7a4d032710bac43b43
Camilla Rosa Freire Sousa <73553875+ho0dlumx@users.noreply.github.com>
2024-01-11 18:33:54 +0100

    Add files via upload

---
+++ code_project.ino
@@ -0,0 +1,43 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+#define LED 26
+
+const char* ssid = "Wi-Fi Maison des Élèves";
+const char* password = "w1f1delaME139";
+
+void setup() {
+  Serial.begin(9600);
+  WiFi.begin(ssid, password);
+  pinMode(LED, OUTPUT);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+    digitalWrite(LED,LOW);
+  }
+  Serial.println("Connected to WiFi");
+  digitalWrite(LED,HIGH);
+  delay(2000);
+  digitalWrite(LED,LOW);
+  
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    String barcode = Serial.readStringUntil('\n'); //
+    if (barcode.length() > 0) {
+      HTTPClient http;
+      //String url = "http://localhost:8000/integration/"+barcode+".html";
+      //String url = "http://IP:8000/integration/"+barcode+".html";
+      String url = "http://linkedin.";
+      http.begin(url);
+      int httpCode = http.GET();
+      if (httpCode > 0) {
+        Serial.println("Page opened: " + url);
+        digitalWrite(LED,HIGH);
+      } else {
+        Serial.println("Error opening page");
+      }
+      http.end();
+    }
+  }
+}
